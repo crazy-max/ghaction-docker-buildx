8d59a028b6d508ebac08bdd9c3618f9e3241c696
CrazyMax <crazy-max@users.noreply.github.com>
2019-10-03 21:43:16 +0200

    Add tests

---
+++ __tests__/installer.test.ts
@@ -0,0 +1,14 @@
+import fs = require('fs');
+import * as installer from '../src/installer';
+
+describe('installer', () => {
+  it('acquires v0.2.2 version of buildx', async () => {
+    const buildx = await installer.getBuildx('v0.2.2');
+    expect(fs.existsSync(buildx)).toBe(true);
+  }, 100000);
+
+  it('acquires latest version of Mage', async () => {
+    const buildx = await installer.getBuildx('latest');
+    expect(fs.existsSync(buildx)).toBe(true);
+  }, 100000);
+});
