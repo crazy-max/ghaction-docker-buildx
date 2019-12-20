935c2a1cd372394521f4c9304356babd562e9d6e
CrazyMax <crazy-max@users.noreply.github.com>
2019-12-20 22:17:36 +0100

    typed-rest-client 1.7.1

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -7,7 +7,7 @@ describe('installer', () => {
     expect(fs.existsSync(buildx)).toBe(true);
   }, 100000);
 
-  it('acquires latest version of Mage', async () => {
+  it('acquires latest version of buildx', async () => {
     const buildx = await installer.getBuildx('latest');
     expect(fs.existsSync(buildx)).toBe(true);
   }, 100000);
